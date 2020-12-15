afb7bbea8eb8d73d1fe429711b1dd810a775023f
Modar Nasser <modar1999@gmail.com>
2020-12-15 18:01:13 +0100

    fixed cmake list

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "Game.hpp"
 
 int main() {
+
+    // load all assets
+    ns::Res::load("assets");
+
+    // configure some inputs buttons
+    ns::Config::Inputs::setButtonKey("droite", sf::Keyboard::Right);
+
     Game g;
     g.run();
+
+    // dont forget to unload all the assets
+    ns::Res::dispose();
     return 0;
 }
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,14 +1,48 @@
 #include "Game.hpp"
 
-Game::Game() :ns::App("NasNas Game Template", 720, 480) {
-    ns::Config::debug = true;
-    addDebugText<std::string>([](){return "World !";}, "Hello", {0, 0});
+
+Game::Game() : ns::App("Ray Cast FPS", {800, 500}){
+    ns::Config::debug = false;
+
+    ///////////////////////////////////////////////////////
+    // create the main scene
+    auto* scene = createScene("main");
+    scene->addLayer("bg", 0);
+
+    // add drawables to it here
+
+    // create the main camera
+    auto* camera = createCamera("main", 0, {0, 0, 800, 500});
+    camera->lookAt(scene);
+    ///////////////////////////////////////////////////////
+
+
+    ///////////////////////////////////////////////////////
+    // create the HUD scene
+    auto* hud = createScene("hud");
+    hud->getDefaultLayer()->add(new sf::CircleShape(50));
+
+    // create the HUD camera
+    auto* hud_cam = createCamera("hud_came", 1);
+    hud_cam->lookAt(hud);
+    ///////////////////////////////////////////////////////
 }
 
 void Game::onEvent(const sf::Event& event) {
     ns::App::onEvent(event);
+    if (event.type == sf::Event::KeyPressed) {
+
+        // create a transition when T is pressed
+        if (event.key.code == sf::Keyboard::T) {
+            auto* tr =  new ns::transition::CircleClose();
+            tr->start();
+            tr->setOnEndCallback([&](){
+                getWindow().setClearColor(sf::Color::Blue);
+                (new ns::transition::CircleOpen())->start();
+            });
+        }
+    }
 }
 
 void Game::update() {
-
 }
