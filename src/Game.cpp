590d780df6a0999b06a78235dac8d94ceb36696f
Modar Nasser <modar1999@gmail.com>
2020-12-15 15:33:08 +0100

    first commit

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,12 +1,12 @@
 #include "Game.hpp"
 
 Game::Game() :ns::App("NasNas Game Template", 720, 480) {
-	ns::Config::debug = true;
-	addDebugText<std::string>([](){return "World !";}, "Hello", {0, 0});
+    ns::Config::debug = true;
+    addDebugText<std::string>([](){return "World !";}, "Hello", {0, 0});
 }
 
 void Game::onEvent(const sf::Event& event) {
-	ns::App::onEvent(event);
+    ns::App::onEvent(event);
 }
 
 void Game::update() {
